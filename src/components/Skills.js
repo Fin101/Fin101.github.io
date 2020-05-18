88d5018a1de16433ec582685beb2aa26585548bc
Finlay Whittington-Devereux <fin.whittington1@gmail.com>
2020-05-18 14:34:30 +0100

    mobile skill icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,20 +15,40 @@ class Skills extends Component {
               <FontAwesomeIcon className="icon" icon={['fab', 'react']} size="4x" />
               <h4>React</h4>
             </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'react']} size="3x" />
+              <h4>React</h4>
+            </div>
             <div className="icon-container">
               <FontAwesomeIcon className="icon" icon={['fab', 'github']} size="4x" />
+              <h4>GitHub</h4>
+            </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'github']} size="3x" />
               <h4>GitHub</h4>
             </div>
             <div className="icon-container">
               <FontAwesomeIcon className="icon" icon={['fab', 'html5']} size="4x" />
               <h4>HTML5</h4>
             </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'html5']} size="3x" />
+              <h4>HTML5</h4>
+            </div>
             <div className="icon-container">
               <FontAwesomeIcon className="icon" icon={['fab', 'js']} size="4x" />
               <h4>JavaScript</h4>
             </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'js']} size="3x" />
+              <h4>JavaScript</h4>
+            </div>
             <div className="icon-container">
               <FontAwesomeIcon className="icon" icon={['fab', 'unity']} size="4x" />
+              <h4>Unity</h4>
+            </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'unity']} size="3x" />
               <h4>Unity</h4>
             </div>
           </div>
@@ -37,20 +57,40 @@ class Skills extends Component {
               <FontAwesomeIcon className="icon" icon={['fab', 'apple']} size="4x" />
               <h4>Apple IOS</h4>
             </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'apple']} size="3x" />
+              <h4>IOS</h4>
+            </div>
             <div className="icon-container">
               <FontAwesomeIcon className="icon" icon={['fab', 'css3']} size="4x" />
+              <h4>CSS3</h4>
+            </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'css3']} size="3x" />
               <h4>CSS3</h4>
             </div>
             <div className="icon-container">
               <FontAwesomeIcon className="icon" icon={['fab', 'npm']} size="4x" />
               <h4>npm</h4>
             </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'npm']} size="3x" />
+              <h4>npm</h4>
+            </div>
             <div className="icon-container">
               <FontAwesomeIcon className="icon" icon={['fab', 'yarn']} size="4x" />
               <h4>yarn</h4>
             </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'yarn']} size="3x" />
+              <h4>yarn</h4>
+            </div>
             <div className="icon-container">
               <FontAwesomeIcon className="icon" icon={['fab', 'python']} size="4x" />
+              <h4>Python</h4>
+            </div>
+            <div className="mobile-icon-container">
+              <FontAwesomeIcon className="icon" icon={['fab', 'python']} size="3x" />
               <h4>Python</h4>
             </div>
           </div>
@@ -59,23 +99,42 @@ class Skills extends Component {
               <i className="icon2 devicon-c-plain" style={{ 'font-size': '4rem' }}></i>
               <h4>C</h4>
             </div>
+            <div className="mobile-icon-container">
+              <i className="icon2 devicon-c-plain" style={{ 'font-size': '3rem' }}></i>
+              <h4>C</h4>
+            </div>
             <div className="icon-container">
               <i className="icon2 devicon-django-plain" style={{ 'font-size': '4rem' }}></i>
               <h4>Django</h4>
             </div>
+            <div className="mobile-icon-container">
+              <i className="icon2 devicon-django-plain" style={{ 'font-size': '3rem' }}></i>
+              <h4>Django</h4>
+            </div>
             <div className="icon-container">
               <i className="icon2 devicon-heroku-plain" style={{ 'font-size': '4rem' }}></i>
-              <h4>heroku</h4>
+              <h4>Heroku</h4>
+            </div>
+            <div className="mobile-icon-container">
+              <i className="icon2 devicon-heroku-plain" style={{ 'font-size': '3rem' }}></i>
+              <h4>Heroku</h4>
             </div>
             <div className="icon-container">
               <i className="icon2 devicon-babel-plain" style={{ 'font-size': '4rem' }}></i>
+              <h4>Babel</h4>
+            </div>
+            <div className="mobile-icon-container">
+              <i className="icon2 devicon-babel-plain" style={{ 'font-size': '3rem' }}></i>
               <h4>Babel</h4>
             </div>
             <div className="icon-container">
               <i className="icon2 devicon-postgresql-plain" style={{ 'font-size': '4rem' }}></i>
               <h4>PostgreSQL</h4>
             </div>
-
+            <div className="mobile-icon-container">
+              <i className="icon2 devicon-postgresql-plain" style={{ 'font-size': '3rem' }}></i>
+              <h4>PostgreSQL</h4>
+            </div>
           </div>
           <div className="skill-icon-row">
             <h4 className="skill-list">Bluma | jQuery | Visual Studio Code | Insomnia | Mongo | TablePlus</h4>
